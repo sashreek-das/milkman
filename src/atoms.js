95f9ef9f2c795afd1f54c82ac1980e7d163877ca
Sashreek-PC <sashreek102003@gmail.com>
2024-08-13 17:44:30 +0530

    atoms added

---
+++ src/atoms.js
@@ -0,0 +1,18 @@
+import { atom } from "recoil";
+
+export const networkAtom = atom({
+    key:"networkAtom",
+    default: 104
+})
+export const jobsAtom = atom({
+    key:"jobsAtom",
+    default: 0
+})
+export const notificationAtom = atom({
+    key:"notificationAtom",
+    default: 0
+})
+export const messagingAtom = atom({
+    key:"messagingAtom",
+    default: 13
+})
